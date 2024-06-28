02ba1f4a5f70fd2eefcd974d820e89e5e0ddc5e2
MAC_KJM <willom2c@naver.com>
2024-06-28 16:16:28 +0900

    Initial commit

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,115 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+interface PostModel {
+  id: number;
+  author: string;
+  content: string;
+  title: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 1234,
+    commentCount: 133,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 혜린',
+    content: '노래 연습하고있는 혜린',
+    likeCount: 1234,
+    commentCount: 133,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙링크 로제',
+    content: '종운 노래중',
+    likeCount: 12334,
+    commentCount: 133,
+  },
+];
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPosts() {
+    return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+    // Array 객체의 find 메서드를 이용하는데 배열의 각 요소(post)에서 id의 값을 외부인자 id와 비교해서
+    // true인 요소(post)만 응답한다
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  @Post()
+  postPost(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+    return post;
+  }
+
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+  }
+}
