019e0b95065962e45807550ac5e3690fe55c7023
MAC_KJM <willom2c@naver.com>
2024-06-29 17:36:05 +0900

    Init Service Layer

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+  id: number;
+  author: string;
+  content: string;
+  title: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 1234,
+    commentCount: 133,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 혜린',
+    content: '노래 연습하고있는 혜린',
+    likeCount: 1234,
+    commentCount: 133,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙링크 로제',
+    content: '종운 노래중',
+    likeCount: 12334,
+    commentCount: 133,
+  },
+];
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts;
+  }
+
+  getPostById(id: number) {
+    const post = posts.find((post) => post.id === +id);
+    // Array 객체의 find 메서드를 이용하는데 배열의 각 요소(post)에서 id의 값을 외부인자 id와 비교해서
+    // true인 요소(post)만 응답한다
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+    return post;
+  }
+
+  updatePost(postId: number, author: string, title: string, content: string) {
+    const post = posts.find((post) => post.id === postId);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === postId ? post : prevPost));
+  }
+
+  deletePost(postId: number) {
+    const post = posts.find((post) => post.id === postId);
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter((post) => post.id !== postId);
+    return postId;
+  }
+}
